bd3db416d8f7a0965b01b31e1a998da255088048
Humendra Pun <xantoshpun@gmail.com>
2026-04-15 07:27:40 +0100

    feat: add @astrojs/sitemap for humendrapun.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://humendrapun.dev',
   compressHTML: true,
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
 });
